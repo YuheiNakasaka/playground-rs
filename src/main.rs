e0703e69f152a8cecc63a106010fecf1c7206df5
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2021-11-27 00:23:40 +0900

    20211127

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,2 @@
-use playground::merkle_tree::Tree;
-
 #[warn(unreachable_patterns)]
-fn main() {
-    let data = vec!["a", "b", "c", "d"];
-    let mut tree = Tree::new(data);
-    tree.build_tree();
-
-    let merkle_pass = tree.get_markle_pass("b".to_string());
-    for pass in &merkle_pass {
-        println!("Hash: {}, Pos: {}", pass[0], pass[1]);
-    }
-
-    let hash = tree.calc(merkle_pass);
-    println!("Expected: {:?}", tree.root);
-    println!("Output: {:?}", hash);
-}
+fn main() {}
--- src/merkle_tree.rs
+++ src/merkle_tree.rs
@@ -203,3 +203,15 @@ mod tests {
         assert_eq!(p1.unwrap().right.unwrap().data, "b");
     }
 }
+
+// 実行例
+// let data = vec!["a", "b", "c", "d"];
+// let mut tree = Tree::new(data);
+// tree.build_tree();
+// let merkle_pass = tree.get_markle_pass("b".to_string());
+// for pass in &merkle_pass {
+//     println!("Hash: {}, Pos: {}", pass[0], pass[1]);
+// }
+// let hash = tree.calc(merkle_pass);
+// println!("Expected: {:?}", tree.root);
+// println!("Output: {:?}", hash);
